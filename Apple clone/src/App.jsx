9e9da062c12eea76c5aa3af104294feecd32c906
Murshid <murshidm2x@gmail.com>
2024-10-28 20:36:52 +0530

    nav added

---
+++ Apple clone/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Highlight from './components/Highlight'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <main className='bg-black'>
+     <Navbar />
+     <Hero />
+     <Highlight />
+   </main>
+  )
+}
+
+export default App
